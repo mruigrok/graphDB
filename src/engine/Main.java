abcb4f40c2aeb0f8570cc3bafff6478be43ae7b3
Matt Ruigrok <mruigrok6@gmail.com>
2019-12-26 10:15:31 -0500

    g

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -3,5 +3,6 @@ package engine;
 public class Main {
     public static void main(String [] args){
         System.out.println("Initializing engine!");
+        System.out.println("Hello");
     }
 }
